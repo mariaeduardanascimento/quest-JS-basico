0f5163fc8a874cd4d268ce9b0efaea3395eba484
Maria Eduarda Nascimento <mariaeduarda6n@gmail.com>
2022-07-14 11:51:29 -0300

    enviando os arquivos para repositorio

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function somar(numero1, numero2){
+        let soma = numero1 + numero2
+        return "A soma dos dois números é: " + soma
+}
+
+let resultadoDaSoma = somar(5,3)
+console.log(resultadoDaSoma)
+
+function subtrair(numero1,numero2){
+    let subtraindoNumeros = numero1 - numero2
+    return "A subtração dos dois números é: " + subtraindoNumeros
+}
+
+let resultadoDaSubtracao = subtrair(8, 5)
+console.log(resultadoDaSubtracao)
+
+function multiplicar(numero1,numero2){
+    let multiplicandoNumeros = numero1 * numero2
+    return "A multiplicação dos dois números é: " + multiplicandoNumeros
+}
+
+let resultadoDaMultiplicacao = multiplicar(6,3)
+console.log(resultadoDaMultiplicacao)
+
+function dividir(numero1, numero2){
+    let dividindoNumeros = numero1 / numero2
+    return "A divisão dos dois números é: " + dividindoNumeros
+}
+
+let resultadoDaDivisao = dividir(16,4)
+console.log(resultadoDaDivisao)
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Quest JS básico
+
+Desafio do modulo de JavaScript básico do curso DevQuest. 
+
+## Sobre o Desafio
+O objetivo dessa quest é a criação de functions para cada operador matematico (soma, subtração, divisão e multiplicação). Depois, deveria ser realizada a chamada da função e um console.log que mostraria o resultado de cada operação.
+
+## Meu processo
+
+Com os conhecimentos adquiridos foi realmente facil fazer o desafio proposto. 
+
+## Tecnologias Utilizadas
+
+- HTML
+- JavaScript
+
+## Autor
+
+- [Maria Eduarda Nascimento](https://www.linkedin.com/in/mariaeduarda-nascimento/)
+
